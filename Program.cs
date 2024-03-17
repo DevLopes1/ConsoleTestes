21fea4009cd86f7b15df7162bc8a09563cc7917e
Davi Lopes <lopesdavi407@gmail.com>
2024-03-16 23:18:42 -0300

    2024.03.16.Build.1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using ConsoleTestes.Programas;
+using System;
+using System.Diagnostics;
+
+Main();
+
+ void Main()
+{
+
+    MenuPrograma.menu();
+}
+
+public class MenuPrograma()
+{
+    static string version = "20240316.Build.1"; // 0000 - ano 00 - mes 00 - dia .Build. - info da build 0 - numero da builda da versao
+    public static void menu()
+    {
+        //Console.WriteLine("Hello World! existem estes programas \n Programa[1]  Programa[2]");
+        Console.Write(
+            "versão: " + MenuPrograma.version + "\n" +
+            "==========ConsoleTestes========== \n" +
+            " Programas/Desafios disponíveis \n" +
+            "Programa - ApenasTeste[1]  Programa - Indefinido[2] \n=>"
+            );
+        string ret = Console.ReadLine();
+
+        switch (ret)
+        {
+            case "1":
+                Console.Write("Inciando programa ApenasTeste... \nDigite uma mensagem para ser exibida!\n=>");
+                string msg = Console.ReadLine();
+                ApenasTeste.teste(msg);
+                break;
+            case "2":
+                break;
+            default:
+                Console.WriteLine("Nenhum programa existe com esta definição!");
+                break;
+        }
+    }
+}
---
+++ Programas/ApenasTeste.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestes.Programas
+{
+    public class ApenasTeste
+    {
+        public static void teste(string t)
+        {
+            Console.WriteLine("Sua menssagem: " + t);
+        }
+    }
+}
